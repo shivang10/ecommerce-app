ed34b50d7b6ebb901ca4aaad3bb660d10040f892
Shivang Garg <shivang10garg@gmail.com>
2021-09-22 12:48:53 +0530

    configures eslint with ts and adds dependencies packages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      Client Side
-    </div>
-  );
-}
+const App: React.FC = () =>{
+    return (
+        <div className="App">
+            Client Side
+        </div>
+    );
+};
 
 export default App;
