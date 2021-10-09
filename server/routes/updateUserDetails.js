c0e957baa143cab9ffd4402af17cabf16e497cc7
Shivang Garg <shivang10garg@gmail.com>
2021-10-09 16:21:42 +0530

    💥💩 update user details

--- server/routes/updateUserDetails.js
+++ server/routes/updateUserDetails.js
@@ -5,13 +5,25 @@ router.route("/:userId").put(async (req, res) => {
     const dataToBeUpdated = req.body;
     const { userId } = req.params;
 
-    User.findOneAndReplace({ _id: userId }, dataToBeUpdated, (err, result) => {
-        if (err) {
-            res.status(200).send(err);
-        } else {
-            res.status(400).send(result);
-        }
-    });
+    User.findOneAndReplace(
+        { _id: userId },
+        { $set: { dataToBeUpdated } },
+        { upsert: true, new: true },
+        (err, result) => {
+            if (err) {
+                res.status(400).send({
+                    message: "Unable to update your details",
+                    status: 400,
+                    response: err
+                });
+            } else {
+                res.status(200).send({
+                    message: "Your details are successfully updated",
+                    status: 200,
+                    response: result
+                });
+            }
+        });
 });
 
 module.exports = router;
