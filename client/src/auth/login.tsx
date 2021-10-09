c551572912e648ee6bb43d1ce785eccb138cc90a
Manish Kumar <manish170898@gmail.com>
2021-10-09 22:21:16 +0530

    signup and login page modified

--- client/src/auth/login.tsx
+++ client/src/auth/login.tsx
@@ -1,5 +1,9 @@
 import React, {useState} from "react";
 
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import PersonIcon from "@mui/icons-material/Person";
+import {Avatar, Box, Button, Grid, InputAdornment, OutlinedInput, Paper, TextField} from "@mui/material";
+import {IconButton} from "@mui/material";
 import {Redirect} from "react-router-dom";
 
 import {UserLoginDetailsInterface} from "./authInterface";
@@ -10,6 +14,10 @@ const Login: React.FC = ()=> {
     const [userDetails, updateUserDetails] = useState<UserLoginDetailsInterface>({
         email: "",
         password: "",
+    });
+
+    const [value, setValue] = useState({
+        showPassword:false
     });
 
     const [loginSuccessful, updateLoginSuccessful] = useState(false);
@@ -38,13 +46,65 @@ const Login: React.FC = ()=> {
         return <Redirect to="/" />;
     }
 
+    const paperStyle ={padding:20, height:"70vh", width:280, margin:"20px auto"};
+
+    const avatarStyle ={backgroundColor:"blue", height:"70px", width:"70px"};
+
+    const handleClickShowPassword = () => {
+        setValue({
+            ...value,
+            showPassword: !value.showPassword,
+        });
+    };
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
     return (
         <div>
-            Login
-            <input onChange={handleChange} value={userDetails.email} name="email" type="text" />
-            <input onChange={handleChange} value={userDetails.password} name="password" type="password" />
-            <button onClick={handleSubmit}>Submit</button>
-            {loginSuccessful && <div> Logged in successfully </div>}
+            <Grid>
+                <Paper elevation={10} style={paperStyle}>
+                    <Grid>
+                        <div style={{display: "flex",  justifyContent:"center", alignItems:"center"}}>
+                            <Avatar style={avatarStyle}><PersonIcon/></Avatar>
+                        </div>
+                        <div style={{display: "flex",  justifyContent:"center", alignItems:"center"}}>
+                            <h2 style={{fontFamily:"sans-serif"}}>Login</h2>
+                        </div>
+                    </Grid>
+                    <Grid marginBottom={1}>
+                        <h3 style={{fontFamily:"sans-serif"}}>Username:</h3>
+                        <TextField name="email" variant="outlined" fullWidth={true} value={userDetails.email} onChange={handleChange} type="text" />
+                    </Grid>
+                    <Grid marginBottom={1}>
+                        <h3 style={{fontFamily:"sans-serif"}}>Password:</h3>
+                        <OutlinedInput
+                            name="password"
+                            type={value.showPassword ? "text" : "password"}
+                            value={userDetails.password}
+                            onChange={handleChange}
+                            fullWidth={true}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {value.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </Grid>
+                    <div style={{display: "flex",  justifyContent:"center", alignItems:"center", marginTop:"20px"}}>
+                        <Button onClick={handleSubmit} variant="outlined" size="large">Login</Button>
+                    </div>
+                    {loginSuccessful && <div style={{fontFamily:"sans-serif",display: "flex",  justifyContent:"center", alignItems:"center", marginTop:"20px", color:"green"}}> Logged in successfully </div>}
+                </Paper>
+            </Grid>
         </div>
     );
 };
--- client/src/auth/signup.tsx
+++ client/src/auth/signup.tsx
@@ -1,5 +1,8 @@
 import React, {useState} from "react";
 
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import PersonIcon from "@mui/icons-material/Person";
+import {Avatar, Button, Grid, IconButton, InputAdornment, OutlinedInput, Paper, TextField} from "@mui/material";
 import {Redirect, useHistory} from "react-router-dom";
 
 import {UserSignupDetailsInterface} from "./authInterface";
@@ -14,6 +17,10 @@ const Signup: React.FC = ()=> {
         email: "",
         phoneNumber: undefined,
         password: "",
+    });
+
+    const [value, setValue] = useState({
+        showPassword:false
     });
 
     const handleSubmit = async () => {
@@ -34,14 +41,70 @@ const Signup: React.FC = ()=> {
         return <Redirect to="/" />;
     }
 
+    const paperStyle ={padding:20, height:"70vh", width:280, margin:"20px auto"};
+
+    const avatarStyle ={backgroundColor:"blue", height:"70px", width:"70px"};
+
+    const handleClickShowPassword = () => {
+        setValue({
+            ...value,
+            showPassword: !value.showPassword,
+        });
+    };
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
     return (
         <div>
-            Signup
-            <input onChange={handleChange} value={userDetails.username} name="username" type="text" />
-            <input onChange={handleChange} value={userDetails.email} name="email" type="text" />
-            <input onChange={handleChange} value={userDetails.phoneNumber} name="phoneNumber" type="number" />
-            <input onChange={handleChange} value={userDetails.password} name="password" type="password" />
-            <button onClick={handleSubmit}>Submit</button>
+            <Grid>
+                <Paper elevation={10} style={paperStyle}>
+                    <Grid>
+                        <div style={{display: "flex",  justifyContent:"center", alignItems:"center"}}>
+                            <Avatar style={avatarStyle}><PersonIcon/></Avatar>
+                        </div>
+                        <div style={{display: "flex",  justifyContent:"center", alignItems:"center"}}>
+                            <h2 style={{fontFamily:"sans-serif"}}>Sign Up</h2>
+                        </div>
+                    </Grid>
+                    <Grid marginBottom={1}>
+                        <h3 style={{fontFamily:"sans-serif"}}>Username:</h3>
+                        <TextField name="username" variant="outlined" fullWidth={true} value={userDetails.username} onChange={handleChange} type="text" />
+
+                        <h3 style={{fontFamily:"sans-serif"}}>Email:</h3>
+                        <TextField name="email" variant="outlined" fullWidth={true} value={userDetails.email} onChange={handleChange} type="text" />
+
+                        <h3 style={{fontFamily:"sans-serif"}}>Phone Number:</h3>
+                        <TextField name="phoneNumber" variant="outlined" fullWidth={true} value={userDetails.phoneNumber} onChange={handleChange} type="number" />
+                    </Grid>
+                    <Grid marginBottom={1}>
+                        <h3 style={{fontFamily:"sans-serif"}}>Password:</h3>
+                        <OutlinedInput
+                            name="password"
+                            type={value.showPassword ? "text" : "password"}
+                            value={userDetails.password}
+                            onChange={handleChange}
+                            fullWidth={true}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {value.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </Grid>
+                    <div style={{display: "flex",  justifyContent:"center", alignItems:"center", marginTop:"20px"}}>
+                        <Button onClick={handleSubmit} variant="outlined" size="large">Sign Up</Button>
+                    </div>
+                </Paper>
+            </Grid>
         </div>
     );
 };
