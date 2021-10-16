142dc2a630c0b883b9f5514c1646f83637cf08c0
Shivang Garg <shivang10garg@gmail.com>
2021-10-16 16:48:28 +0530

    🐛 changes axios method for get order details

--- server/routes/previousOrders.js
+++ server/routes/previousOrders.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/user");
 
-router.route("/").get((req, res) => {
+router.route("/").post((req, res) => {
     const { userId } = req.body;
 
     User.findById(
